be99e642e2c5c8af989137b4adcd8b30bce52bf7
Meystergod <malahov_gleb@mail.ru>
2023-03-15 15:55:04 +0500

    add func for creating vsa hex by wlan id

--- util.go
+++ util.go
@@ -161,3 +161,17 @@ func AddVSAInt(p *radius.Packet, vendor uint32, attribute uint8, value int) {
 	vsa, _ := radius.NewVendorSpecific(vendor, attr)
 	p.Add(rfc2865.VendorSpecific_Type, vsa)
 }
+
+func CreateHexVSAByWlanID(value string) (hex []byte) {
+	if len(value) > 249 {
+		return
+	}
+	bytes := []byte(value)
+	attr := make([]byte, 2+len(bytes))
+	attr[0], attr[1] = byte(WimarkIdentifierWLANType), byte(len(attr))
+	copy(attr[2:], bytes)
+	hex = make([]byte, 4+len(attr))
+	binary.BigEndian.PutUint32(hex, VendorWimark)
+	copy(hex[4:], attr)
+	return
+}
